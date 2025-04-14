d8963615b4cadac4bcbd542961fc314a0aa6adea
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2025-04-14 08:58:57 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,28 @@
+public class Pyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++){
+            for (int j = n; j >=i; j--) {
+                System.out.print(" ");
+                
+            }
+            for(int k=0; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            }
+
+            for(int i=0; i<=n; i++){
+                for(int j=0; j<=i; j++){
+                    System.out.print(" ");
+                }
+                for(int k=n; k>=i; k--){
+                    System.out.print("* ");
+                }
+                System.out.println( );
+
+        }
+    }   
+}
+    
+
