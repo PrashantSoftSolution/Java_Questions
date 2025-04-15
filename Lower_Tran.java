c370b70c22a901d7b292b6c1f6f4a3a0234174d7
Prashant Srivastava <129353763+PrashantSoftSolution@users.noreply.github.com>
2025-04-15 10:15:43 +0530

    Add files via upload

---
+++ Lower_Tran.java
@@ -0,0 +1,12 @@
+public class Lower_Tran {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<=n; i++){
+            for(int j=n; j>=i; j--){
+                System.out.print(" * ");
+            }
+            System.out.println( );
+        }
+    }
+    
+}
